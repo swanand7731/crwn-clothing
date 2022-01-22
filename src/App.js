b57891136a91a96ef33d267ffb08e31443ad9451
Swan <28915682+swanand7731@users.noreply.github.com>
2022-01-22 23:34:51 +0530

    Updating the App with Collection Preview, Item Page and shop page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from './pages/hompage/homepage.component';
-
-const HatsPage = () => <div>Hats Page</div>;
-
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Router>
     </div>
   );
