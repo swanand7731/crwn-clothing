6b583c75e65d45096ef95effbafd538a2ae5e9c1
Swan <28915682+swanand7731@users.noreply.github.com>
2022-01-23 15:56:51 +0530

    Adding Links to Shop Page, home Page and adding separate header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Router>
