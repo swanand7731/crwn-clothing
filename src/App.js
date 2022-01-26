7d5bb1408544848d19531e0f18cf55e2ebe28c8d
Swan <28915682+swanand7731@users.noreply.github.com>
2022-01-26 10:03:27 +0530

    added code to redirect user if signed-in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import HomePage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -44,11 +44,22 @@ class App extends React.Component {
           <Header />
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInPage />
+            }
+          />
         </Router>
       </div>
     );
   }
 }
 
-export default connect(null, { setCurrentUser })(App);
+const mapStateToProps = ({ user }) => {
+  return {
+    currentUser: user.currentUser,
+  };
+};
+export default connect(mapStateToProps, { setCurrentUser })(App);
